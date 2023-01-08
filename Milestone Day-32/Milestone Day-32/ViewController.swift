d2cf4e63a1fab1460a0eab3c1f7839f739790d5c
Evgeniia Zorich <vasileva.evg.al@gmail.com>
2023-01-08 19:19:21 +0100

    initial commit

---
+++ Milestone Day-32/Milestone Day-32/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone Day-32
+//
+//  Created by Евгения Зорич on 08.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
